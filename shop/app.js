1df4d479a9ce8727542f5d4255f0dbf8a92a3de9
hossam <hossa@uop.edu.jo>
2025-03-26 13:01:58 +0300

    middleware+routing

--- shop/app.js
+++ shop/app.js
@@ -1,9 +1,18 @@
+//middleware MVC folders app.js routing
 const express=require('express');
+const indexRouter = require("./routes/index");
+let name = "";
 const app=express();
+app.set("view engine","ejs");
+app.set("views","views");
+const testMiddleware = (req,res,next) => {
+  name = "Hossam";
+  next();
+};
 
-app.get("/",(req,res)=>{
-res.send("<h1>First Application</h1>")
-  });
+app.use(testMiddleware);
+//app.use(indexRouter);
+
 app.listen(8080);
 
 // open the browser and write URL http://localhost:8080/
