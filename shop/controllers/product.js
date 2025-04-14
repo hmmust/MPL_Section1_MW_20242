25c7cf70c2bb7d20c3918b316903a484145449e0
hossam <hossa@uop.edu.jo>
2025-04-14 13:30:00 +0300

    views

--- shop/controllers/product.js
+++ shop/controllers/product.js
@@ -1,15 +1,16 @@
+const products =['Water','Banana','Juice','PC']
 const getProduct= (req,res)=>{
-        res.render("addproduct",{title:'Add Product'});
+        res.render("product/addproduct",{title:'Add Product'});
  };
 const getProducts= (req,res)=>{
-    //res.render("index.ejs");
-    res.end("Products");
+    res.render("product/viewproducts.ejs",{title:'View Products', prods: products});
     };
 const postProduct= (req,res)=>{
-        res.write("Data received!");
-        res.write(req.body.name);
-        res.write(req.body.type);
-        res.end(req.body.price);
+        res.render("product/viewproduct",{title:'View Product',
+                name:req.body.name,
+                price:req.body.price,
+                type:req.body.type
+        });
        };
 const getProductsId= (req,res)=>{
         res.write(req.params.pid+"<br>");
